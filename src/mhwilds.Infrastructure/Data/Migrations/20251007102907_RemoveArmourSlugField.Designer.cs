c4e4e117d4ee96cdf55e42fbac49e26e88a51eb0
niranjan-gurung <gurung5130@gmail.com>
2026-04-20 12:13:59 +0100

    armour slug removed. secrets replaced with .env setup for local dev.

--- src/mhwilds.Infrastructure/Data/Migrations/20251007102907_RemoveArmourSlugField.Designer.cs
+++ src/mhwilds.Infrastructure/Data/Migrations/20251007102907_RemoveArmourSlugField.Designer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using mhwilds.Infrastructure.Data.Context;
@@ -11,9 +12,11 @@ using mhwilds.Infrastructure.Data.Context;
 namespace mhwilds.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251007102907_RemoveArmourSlugField")]
+    partial class RemoveArmourSlugField
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/mhwilds.Infrastructure/Data/Migrations/20251007102907_RemoveArmourSlugField.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mhwilds.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveArmourSlugField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Slug",
+                table: "Armours");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Slug",
+                table: "Armours",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
